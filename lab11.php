d865a9a4c7e64074de4a2dd0b7f5b777bbbebc12
LausThech <70843608+LausThech@users.noreply.github.com>
2023-03-20 11:43:12 +0300

    Add files via upload

---
+++ lab11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<form action="lab11script.php" method="post">
+<input type="text" name="class">
+<input type="text" name="food">
+<input type="text" name="location">
+<input type="text" name="sign">
+<button>Вычислить</button>
+</form>
+</form>
+</body>
+</html>
