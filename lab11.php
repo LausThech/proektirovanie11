4721474ce3c1e554f825f39afbd0c70b7eb4c635
LausThech <70843608+LausThech@users.noreply.github.com>
2023-04-03 14:31:51 +0300

    Update lab11.php

--- lab11.php
+++ lab11.php
@@ -6,9 +6,9 @@
 <body>
 <form action="lab11script.php" method="post">
 <input type="text" name="class" placeholder="class - Cat,Horse,Dog">
-<input type="text" name="food" placeholder="Еда">
-<input type="text" name="location" placeholder="Локация">
-<input type="text" name="sign" placeholder="Уникальное">
+<input type="text" name="food" placeholder="food">
+<input type="text" name="location" placeholder="location">
+<input type="text" name="sign" placeholder="sign">
 <button>Вычислить</button>
 </form>
 </form>
